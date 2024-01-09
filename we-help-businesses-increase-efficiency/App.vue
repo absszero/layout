6022e61cf5b4e8e4e91a33763bc7b0c421680610
Adrian Chen <absszero@gmail.com>
2024-01-09 15:35:10 +0800

    no message

--- we-help-businesses-increase-efficiency/App.vue
+++ we-help-businesses-increase-efficiency/App.vue
@@ -1,98 +1,271 @@
 <!-- https://mobileapp.nicepage.io/Page-2.html?version=c157bc4d-d4e4-46eb-bff4-a604df304a68 -->
 <template>
-  <div class="container mx-auto p-x-10 lg:p-x-0">
-    <section class="jumbotron" flex="md:~">
-      <div>
+  <div class=" mx-auto">
+    <section class="jumbotron"
+    flex="md:~"
+    >
+      <div
+        w="md:4/7"
+      >
         <h1
-        text="size-5xl md:size-6xl md:base/16 lg:size-8xl lg:base/22" font-bold
-        m="y-5 t-10 md:t-30"
-        >Create<br> & deliver</h1>
+        font="bold"
+        text="size-6xl lg:size-8xl24"
+        p="t-10 md:t-30"
+        >Create<br>& deliver</h1>
         <h2
         capitalize
         font="light"
         text="white"
         m="b-10"
         >Real Experience</h2>
-        <a bg-red-500 w-28 text-center block mb-10 p-y-3 p-x-5 uppercase text-size-xs font-semibold href="">see more</a>
+        <a
+        block uppercase
+        w="36"
+        m="b-10"
+        p="y-3 x-4"
+        bg="#f70647"
+        hover:bg="#de0540"
+        un-text="center size-sm"
+        font="semibold" href="">see more</a>
       </div>
       <img
-       w="2/3 lg:1/3"
-       m="md:t-15"
+       w="5/7 md:3/5 lg:2/5"
+       p="md:t-15"
        mx-auto src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/60700b8c43095d66ada6928d/579161.png" alt="">
     </section>
     <section class="12">
-      <div class="01">
-        <img src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/ef005f3d59d953ce906b4031/67732.jpg" alt="">
-        <div>
-          <h1>01</h1>
-          <h2>An Inspirational Technology</h2>
-          <p>Phasellus scelerisque sed leo quis gravida. Fusce lobortis libero ut arcu blandit pharetra.</p>
+      <div class="01"
+      flex="md:~ md:justify-between"
+      >
+        <div
+          mx-a
+          md:relative
+          md:-top-25
+          w="md:3/5"
+        >
+          <img
+            object-cover
+            size:full
+            src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/ef005f3d59d953ce906b4031/67732.jpg" alt="">
+        </div>
+        <div
+        w="2/3 md:3/7"
+        m="md:l-10"
+        >
+          <h1
+          text="size-8xl md:size-9xl"
+          m="y-20"
+          >01</h1>
+          <h2
+          m="b-5"
+          >An Inspirational Technology</h2>
+          <p
+          m="b-30"
+          >Phasellus scelerisque sed leo quis gravida. Fusce lobortis libero ut arcu blandit pharetra.</p>
         </div>
       </div>
-      <div class="02">
-        <div>
-          <h1>02</h1>
-          <h2>Quickly pick up a new skill </h2>
-          <p>How much time do you think that you get to make a good impression with a potential customer? If you’re
+      <div class="d02"
+      flex="md:~ md:justify-between"
+      >
+        <div
+        p="md:l-10"
+        w="md:3/7"
+        >
+          <h1
+          text="size-8xl md:size-9xl"
+          m="y-20"
+          >02</h1>
+          <h2
+          m="b-5"
+          >Quickly pick up a new skill </h2>
+          <p
+          m="b-30"
+          >How much time do you think that you get to make a good impression with a potential customer? If you’re
             talking about interacting with them online, you have less than three seconds.</p>
         </div>
-        <img src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/d1dd2654458d52c7b8db60c9/OS5KJE1.png" alt="">
+        <div
+          class="smart-phone"
+          size="md:4/7"
+        >
+          <img
+        object-cover
+        size="full"
+        src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/d1dd2654458d52c7b8db60c9/OS5KJE1.png" alt="">
+        <img
+        float-right
+        relative
+        right="-7"
+        top="-21 md:12"
+        object-cover
+        size="full"
+        src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/5e6b72477b475653b72fd837/dots.png" alt="">
+        </div>
       </div>
     </section>
-    <section class="counts">
-      <h1>Doing The Right Things</h1>
-      <ul>
-        <li v-for="count in counts">
+    <section class="counts"
+      uppercase
+      bg="white "
+      text="#43005d center"
+    >
+      <h1
+        p="y-20"
+        font="bold"
+        text="size-3xl md:size-4xl"
+      >Doing The Right Things</h1>
+      <ul
+      mx-a
+      p="b-15"
+      w="md:4/5 lg:full"
+      flex="md:~ md:wrap md:justify-between"
+      >
+        <li
+          p="b-15 md:x-15"
+        v-for="count in counts">
           <div>
-            <h3>{{ count.numbers }}</h3>
-            <span>{{ count.title }}</span>
+            <h3
+            font="bold"
+            text="#f70647 size-5xl md:size-6xl"
+            >{{ count.numbers }}</h3>
+            <span
+            font="bold"
+            text=" size-sm"
+            >{{ count.title }}</span>
           </div>
         </li>
       </ul>
     </section>
-    <section class="form">
-      <div>
-        <h2>Dream, train and activate</h2>
-        <p>Justis pede vixtu, aliquet vel fringilla, pellentes arcu eget mus vix</p>
+    <section class="form"
+      p="y-25"
+      flex="md:~ md:justify-between"
+    >
+      <div
+        w="md:3/7"
+        p="x-5"
+        m="b-15"
+      >
+        <h2
+        text="size-4xl base/10 md:size-5xl md:base/13"
+        tracking-wider
+        font="semibold"
+        p="t-15"
+        m="b-5"
+        >Dream, train and activate</h2>
+        <p
+        text="size-16px"
+        >Justis pede vixtu, aliquet vel fringilla, pellentes arcu eget mus vix</p>
       </div>
-      <form action="">
-        <input type="text" placeholder="Name">
-        <input type="email" placeholder="Email">
-        <input type="text" placeholder="Message">
-        <button>Submit</button>
+      <form
+      w="md:3/7"
+      action="">
+        <input
+          text="size-16px"
+          bg="transparent"
+          p="2"
+          m="b-3"
+          w="full"
+          border="3 #f70647"
+        type="text" placeholder="Name">
+        <input
+          text="size-16px"
+          bg="transparent"
+          p="2"
+          m="b-3"
+          w="full"
+          border="3 #f70647"
+        type="email" placeholder="Email">
+        <textarea
+          text="size-16px"
+          bg="transparent"
+          p="2"
+          m="b-3"
+          w="full"
+          rows="4"
+          border="3 #f70647"
+        type="text" placeholder="Message"></textarea>
+        <button
+        uppercase
+        font="bold"
+        text="size-sm"
+        bg="#f70647"
+        p="x-7 y-2"
+        >Submit</button>
       </form>
     </section>
-    <section class="03">
-      <div>
-        <img src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/66ee030b4117564e874269a7/grcell2.jpg" alt="">
+    <section class="03"
+      flex="md:~ wrap"
+      p="x-0"
+    >
+      <div
+        w="full md:1/2"
+      >
+        <img
+        object-cover
+        size="full"
+        src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/66ee030b4117564e874269a7/grcell2.jpg" alt="">
       </div>
-      <div>
-        <h1>03</h1>
-        <h2>Userfriendly Design</h2>
+      <div
+        p="y-20 x-10"
+        w="md:1/2"
+      >
+        <h1
+          text="size-8xl md:size-9xl"
+          m="y-15"
+        >03</h1>
+        <h2
+          text="md:size-1.8rem"
+          m="b-5"
+        >Userfriendly Design</h2>
         <p>
           How much time do you think that you get to make a good impression with a potential customer? If you’re talking
           about interacting with them online, you have less than three seconds.
         </p>
-        <img src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/e8e7290111aa5533acfd6f40/Untitled6.jpg" alt="">
       </div>
-      <div>
-        <h2>what we do</h2>
-        <p>
+      <div
+        w="md:1/2"
+        bg="#f2f2f2"
+      >
+        <h2
+          text="center md:left size-5xl md:size-3xl"
+          p="x-15 y-15 md:x-10 md:y-5"
+        >what we do</h2>
+        <p
+          p="x-15 md:x-10"
+          text="black"
+        >
           While you could create a separate version of your website that works well on mobile devices from the one people
           see on desktop, for most businesses the better option is to make one website that’s responsive.
         </p>
+        <img
+          w="full"
+        src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/e8e7290111aa5533acfd6f40/Untitled6.jpg" alt="">
       </div>
-      <div>
-        <img src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/193c299b8dcc56ccafc40e7b/Untitled2.jpg" alt="">
+      <div
+        w="md:1/2"
+      >
+        <img
+        object-cover
+        size="full"
+        src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/193c299b8dcc56ccafc40e7b/Untitled2.jpg" alt="">
       </div>
     </section>
-    <section class="get-the-app">
-      <h2>Get the App</h2>
-      <p>
+    <section
+      text="center"
+    class="get-the-app">
+      <h2
+      p="y-15"
+      text="size-5xl"
+      >Get the App</h2>
+      <p
+      text="#e2a8f8"
+      m="b-10"
+      >
         In vitae velit luctus, imperdiet augue et, elementum dui. Sed sit amet nisi nulla. In ut diam id lectus efficitur
         efficitur quis vitae felis. Suspendisse euismod leo sapien, sit amet accumsan quam fermentum nec.
       </p>
-      <img src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/752054dc06b85c85ae62733f/h5img11.png" alt="">
+      <img
+      m="xa"
+      p="b-15"
+      src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/752054dc06b85c85ae62733f/h5img11.png" alt="">
     </section>
   </div>
 </template>
@@ -125,14 +298,15 @@ html {
 }
 
 body {
+  font-size: 18px;
   color: white;
   background-color: #43005d;
 }
 
 h1 {
+  line-height: 1.25;
   font-size: 5rem;
   text-transform: uppercase;
-  line-height: 3rem;
   font-weight: lighter;
 }
 
@@ -145,8 +319,36 @@ h2 {
 
 }
 
+section.jumbotron {
+  /* background-image: url('https://images01.nicepage.com/c461c07a441a5d220e8feb1a/b5aebdc251425e8aa6997dbc/bggr.jpg'); */
+  background-image: linear-gradient(0deg, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), url("//images01.nicepage.com/c461c07a441a5d220e8feb1a/b5aebdc251425e8aa6997dbc/bggr.jpg");
+  background-position: 50% 50%;
+  background-size: cover;
+}
+
+section.form {
+  background-image: linear-gradient(0deg, rgba(67,0,93,0.7), rgba(67,0,93,0.7)), url("//images01.nicepage.com/c461c07a441a5d220e8feb1a/3a6e5f4b71085e33b7ff41b2/closeupofthreebusinesspeoplehandsusingtheirsmartphoneswhileridingtheelevator_swgbe9_9r__F0000.jpg");
+  background-position: 50% 50%;
+  background-size: cover;
+}
+
 section {
   /* border: 1px solid white; */
+  padding: 0 2rem;
+}
+
+section.get-the-app {
+  background-image: url("//images01.nicepage.com/c461c07a441a5d220e8feb1a/aea417978fcd526793aad9d9/xc.jpg");
+  background-position: 50% 50%;
+  background-size: cover;
+}
+
+div.smart-phone {
+  background-image: url('https://images01.nicepage.com/c461c07a441a5d220e8feb1a/2a86674886725d6dbeabfda2/Untitled6.png');
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-size: 90%;
 }
 
 </style>
+
